465a12d729d9829c64cdfd17d149991c0f61a4b1
Arihin <arihin.slava@yandex.ru>
2022-10-21 19:46:34 +0300

    Домашняя работа Переменные2

---
+++ src/Main.java
@@ -0,0 +1,42 @@
+public class Main {
+    public static void main(String[] args) {
+
+        byte bananas = 100;
+        System.out.println("Бананов " + bananas + "Кг" );
+
+        float sugar = 3;
+        float onePortion = sugar / 4;
+        System.out.println("Одна порция весит " + onePortion + " кг");
+
+        byte a = 1;
+        short b = 1;
+        int c = 1;
+        int d = a + b + c;
+        System.out.println(d);
+
+        float g = a + 1f;
+        System.out.println(g);
+
+       byte aplles = 7;
+       System.out.println(aplles);
+
+       int gold = 100;
+       System.out.println(gold);
+
+       short A = 1200;
+       System.out.println(A);
+
+       long ice = 5000;
+       System.out.println(ice);
+
+       double v = 1.5687;
+       System.out.println(v);
+
+       float way = aplles + 1f;
+       System.out.println(way);
+
+
+    }
+
+
+}

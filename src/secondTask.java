e6fb2552af7c381fd6d420c25abd4f6989739066
Arihin <arihin.slava@yandex.ru>
2022-10-22 19:43:12 +0300

    Домашняя работа Переменные2

---
+++ src/secondTask.java
@@ -0,0 +1,33 @@
+public class secondTask {
+    public static void main(String[] args) {
+        double a = 27.12;
+        System.out.println(a);
+
+        short b = 987;
+        System.out.println(b);
+        short c = 678;
+        System.out.println(c);
+        int A = 965;
+        System.out.println(A);
+        int B = 549;
+        System.out.println(B);
+        Byte C = 2;
+        System.out.println(C);
+        long d = 786;
+        System.out.println(d);
+        boolean box = false;
+        System.out.println(box);
+        short f = -159;
+        System.out.println(f);
+        int h = 27897;
+        System.out.println(h);
+        byte j = 67;
+        System.out.println(j);
+
+
+    }
+}
+
+
+
+
